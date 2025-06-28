1511069088c75a9317f1fb130a0ba9f98d0ef89a
Alex Fomenko <alexfomg@gmail.com>
2025-06-28 17:11:07 +0500

    Auto deb build

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ParentalControl
 
-Parental Control
+According to the schedule, it does not allow you to run the programs specified 
+in the configuration file.
---
+++ build_deb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Fomenko A V  2025 (c)
+
+cp -vf parentalcontrol deb/parentalcontrol/usr/bin/parentalcontrol
+
+chmod +x deb/parentalcontrol/usr/bin/parentalcontrol
+
+echo "Enter Version (x.x-x.x):"
+read VERSION
+
+rm -rfv deb/parentalcontrol/DEBIAN/control 
+
+CONTROL_FILE="Package: parentalcontrol
+Version: $VERSION
+Section: misc
+Architecture: all
+Priority: optional
+Depends: python3, python3-psutil
+Maintainer: Alex <alexfomg@gmail.com>
+Description: Parental Control
+Installed-Size: $(du -sb deb/parentalcontrol  | grep -o '^[0-9]*')" 
+
+echo "$CONTROL_FILE" > deb/parentalcontrol/DEBIAN/control 
+
+cd deb/
+./make_deb.sh 
